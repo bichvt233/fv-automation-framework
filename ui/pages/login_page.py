24dc3769609aab14693f267a77acf18fba908541
bich_vt <bich_vt@flinters.vn>
2026-05-06 17:15:36 +0700

    refactor: login page using base page + locator separation

--- ui/pages/login_page.py
+++ ui/pages/login_page.py
@@ -1,15 +1,11 @@
-class LoginPage:
-    def __init__(self, page):
-        self.page = page
+from core.base_page import BasePage
+class LoginPage(BasePage):
+    # Locator tách riêng
+    USERNAME_INPUT = "#user-name"
+    PASSWORD_INPUT = "#password"
+    LOGIN_BUTTON = "#login-button"
 
-    def login(self, username, password):
-        # muốn chuyên URL này vào một biến là base_url để dễ maintain hơn
-        base_url = "https://www.saucedemo.com/"
-        self.page.goto(base_url)
-        self.page.locator("#user-name").wait_for()
-        self.page.fill("#user-name", username)
-
-        self.page.locator("#password").wait_for()
-        self.page.fill("#password", password)
-        self.page.click("#login-button")
-        
+    def login(self, username: str, password: str):
+        self.fill(self.USERNAME_INPUT, username)
+        self.fill(self.PASSWORD_INPUT, password)
+        self.click(self.LOGIN_BUTTON)
