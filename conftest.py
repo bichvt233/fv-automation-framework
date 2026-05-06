18141a6960f0ad5de62d8beef44a62f68a8958f0
BichVT <138669177+bichvt233@users.noreply.github.com>
2026-05-06 17:03:37 +0700

    Thêm screenshot khi fail

--- conftest.py
+++ conftest.py
@@ -31,3 +31,20 @@ def browser_type_launch_args():
     return {
         "headless": True   # 🔥 QUAN TRỌNG
     }
+
+import pytest
+import allure
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    outcome = yield
+    rep = outcome.get_result()
+
+    if rep.when == "call" and rep.failed:
+        page = item.funcargs.get("page")
+        if page:
+            allure.attach(
+                page.screenshot(),
+                name="failure-screenshot",
+                attachment_type=allure.attachment_type.PNG
+            )
